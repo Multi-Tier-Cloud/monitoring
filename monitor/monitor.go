583b70a4467733d6142115dd2516e815fc5f894f
Ubuntu <29393345+hivanco@users.noreply.github.com>
2020-03-23 02:37:50 -0400

    debug mode for printing out addresses

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -4,6 +4,7 @@ import (
     "context"
     "errors"
     "fmt"
+    "os"
     "time"
 
     "github.com/libp2p/go-libp2p/p2p/protocol/ping"
@@ -13,13 +14,18 @@ import (
 
 // collect pings all peers in the monitor node's Peerstore to collect
 // performance data. For now it prints out what it finds
-func collect(node *p2pnode.Node) {
-    // Setup new timer to allow one ping per second
-    timer := time.NewTimer(1 * time.Second)
+func collect(node *p2pnode.Node, debug bool) {
     // Loop infinitely
     for {
         // Get peer in Peerstore
         for _, id := range node.Host.Peerstore().Peers() {
+            // Print address info in debug mode
+            if debug {
+                fmt.Println("\nAddr:", node.Host.Peerstore().PeerInfo(id).Addrs)
+            }
+
+            // Setup new timer to allow one ping per second
+            timer := time.NewTimer(1 * time.Second)
             // Block until timer fires
             <-timer.C
             // Ping and print result
@@ -35,6 +41,12 @@ func collect(node *p2pnode.Node) {
 }
 
 func main() {
+    // Check for debug mode
+    debug := false
+    if len(os.Args) == 2 && os.Args[1] == "debug" {
+        debug = true
+        fmt.Println("Debug mode")
+    }
 
     // Setup node
     ctx := context.Background()
@@ -48,6 +60,6 @@ func main() {
     }
 
     fmt.Println("Starting data collection")
-    collect(&node)
+    collect(&node, debug)
     panic(errors.New("Monitor node exitted monitor loop"))
 }
