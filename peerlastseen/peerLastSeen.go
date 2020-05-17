99fd8868da52e0c41adbc22f0ba331d57f29ff45
Thomas Lin <t.lin@mail.utoronto.ca>
2020-05-17 17:53:58 -0400

    Implemented "PeerLastSeen" to track when peers were last seen   - If they haven't been seen for x amount of time (configurable), then     it is removed from memory and an optional callback is invoked.   - Made corresponding changes in ping-monitor to use this structure.     Currently hard-coded to 10 minutes to expiry, and the callback removes     the corresponding metric from Prometheus.   - Resolves #5

---
+++ peerlastseen/peerLastSeen.go
@@ -0,0 +1,123 @@
+package peerlastseen
+
+import (
+    "errors"
+    "sync"
+    "time"
+
+    "github.com/libp2p/go-libp2p-core/peer"
+)
+
+type PeerLastSeenCB func(peer.ID)
+
+// TODO: Merge PeerLastSeen code into common/p2putil?
+// Similar to libp2p's 'identify' package's ObservedAddr, but for IDs rather
+// than specific multiaddresses
+type PeerLastSeen struct {
+    mutex           sync.RWMutex
+    lastSeen        map[peer.ID]time.Time
+
+    // Peers last seen longer than the timeout duration will be removed.
+    timeout         time.Duration
+
+    // Indicates if the background peer clean-up goroutine is active.
+    // The info in this data structure should be considered stale
+    // if the goroutine is offline.
+    cleanupActive   bool
+
+    // Optional callback invoked by background clean-up goroutine whenever
+    // a peer's timeout is up. Up to user to ensure its thread-safe.
+    callback        PeerLastSeenCB
+}
+
+func NewPeerLastSeen(dur time.Duration, cb PeerLastSeenCB) (*PeerLastSeen, error) {
+    if (dur <= 0) {
+        return nil, errors.New("Unable to set timeout to a value <= 0")
+    }
+
+    pls := &PeerLastSeen {
+        lastSeen:       make(map[peer.ID]time.Time),
+        timeout:        dur,
+        cleanupActive:  false,
+        callback:       cb,
+    }
+
+    // Start background goroutine and wait for it to come up
+    go pls.autoRemove()
+    for pls.cleanupActive == false {
+        time.Sleep(time.Millisecond)
+    }
+
+    return pls, nil
+}
+
+func (pls *PeerLastSeen) UpdateLastSeen(id peer.ID) {
+    now := time.Now()
+
+    pls.mutex.Lock()
+    defer pls.mutex.Unlock()
+
+    pls.lastSeen[id] = now
+}
+
+// Returns timestamp of when peer with given 'id' was last seen
+// Use absolute time rather than duration in case 'id' is not in
+// the map and we return the default 0-value
+func (pls *PeerLastSeen) LastSeen(id peer.ID) (time.Time, error) {
+    if pls.cleanupActive == false {
+        return time.Time{}, errors.New("Background clean-up goroutine for PeerLastSeen is offline")
+    }
+
+    pls.mutex.RLock()
+    defer pls.mutex.RUnlock()
+
+    timestamp := pls.lastSeen[id]
+    return timestamp, nil
+}
+
+// Creates a background goroutine to automatically clear peers
+// that are older than the 'timeout' value set
+func (pls *PeerLastSeen) autoRemove() {
+    if pls.cleanupActive {
+        return // This should not be called twice...
+    }
+
+    pls.cleanupActive = true
+    defer func() {
+        // If this goroutine ever crashes, all other methods should fail
+        pls.cleanupActive = false
+    }()
+
+    // Currently hard-coded to check every second... is this too much?
+    // TODO: Think about how to make this more scalable using info from
+    //       the lastSeen map... perhaps manually set new ticks each time.
+    ticker := time.NewTicker(time.Second)
+    defer ticker.Stop()
+
+    for {
+        <-ticker.C
+
+        // Put cleanup process in a separate goroutine so the mutex defer
+        // is local within it, otherwise the defer will apply to this
+        // scope, and will never be Unlock()'d so long as its running.
+        //
+        // Could manually call Unlock(), but there's a chance of
+        // a panic/crash, and Unlock() would never be called.
+        go func() {
+            pls.mutex.Lock()
+            defer pls.mutex.Unlock()
+
+            for id, ts := range(pls.lastSeen) {
+                if time.Since(ts) > pls.timeout {
+                    delete(pls.lastSeen, id)
+
+                    if pls.callback != nil {
+                        go pls.callback(id)
+                    }
+                }
+            }
+        }()
+    }
+}
+
+
